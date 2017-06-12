7eb35fed4106b51b98f4bb480fb59cfb512b5d99
Hanley, Thomas <Thomas.Hanley@moodys.com>
2017-06-12 09:57:05 -0400

    Adding no animation module, fixing flex span issue for toolbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule, } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
@@ -9,12 +10,12 @@ import { MaterialModule } from '@angular/material';
 import { SharedModule } from '../shared/shared.module';
 import { metaReducer } from './metaReducer/index';
 
-//Add effects here
 @NgModule({
   imports: [
     StoreModule.provideStore(metaReducer),
     MaterialModule,
-    SharedModule
+    SharedModule,
+    NoopAnimationsModule
   ],
   declarations: [
       ],
--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -2,16 +2,10 @@ import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'eg-toolbar',
-  template:
-  `<md-toolbar color="primary">
-      <button md-icon-button (click)="openSidenav.emit()">
-        <md-icon>menu</md-icon>
-      </button>
-      <ng-content></ng-content>
-    </md-toolbar>`
+  templateUrl: 'toolbar.component.html',
+  styleUrls: ['toolbar.component.css'],
 
 })
 export class ToolbarComponent {
      @Output() openSidenav = new EventEmitter();
 }
-
