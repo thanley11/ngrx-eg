42f167986df441304190bf20319ea0bad5e80eb0
Hanley, Thomas <Thomas.Hanley@moodys.com>
2017-09-19 08:44:26 -0400

    Removing ngbmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, BaseRequestOptions } from '@angular/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MaterialModule } from '@angular/material';
 import { StoreModule } from '@ngrx/store';
 
